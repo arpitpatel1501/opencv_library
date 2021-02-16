d2234bfb79877a5c467ac10dd50be33c1443b4e6
Your Name <you@example.com>
2021-02-16 09:25:46 +0530

    opencv bin and lib libraries

---
+++ include/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/arpit/AppData/Roaming/npm/node_modules/opencv4nodejs/node_modules/opencv-build/opencv/opencv/include
+# Build directory: C:/Users/arpit/AppData/Roaming/npm/node_modules/opencv4nodejs/node_modules/opencv-build/opencv/build/include
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
